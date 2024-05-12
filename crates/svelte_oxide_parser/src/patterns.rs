34989c281abc70c4c5896e0433fa5c03a4137911
Dilshad <a-rustacean@outlook.com>
2024-05-12 10:12:24 +0530

    chore(parser): restart from scratch

--- crates/svelte_oxide_parser/src/patterns.rs
+++
@@ -1,6 +0,0 @@
-use lazy_static::lazy_static;
-use regex::Regex;
-
-lazy_static! {
-    pub static ref REGEX_WHITESPACE: Regex = Regex::new(r"\s").unwrap();
-}
--- crates/svelte_oxide_parser/src/utils/html.rs
+++
@@ -1,7 +0,0 @@
-// TODO: make it working
-pub fn decode_character_references(
-    html: &str,
-    _is_attribute_value: bool,
-) -> String {
-    html.to_string()
-}
