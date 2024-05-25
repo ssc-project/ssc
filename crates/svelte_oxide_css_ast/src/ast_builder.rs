26a848973fd341958fb5372d9fb57e1a345f79ef
Dilshad <a-rustacean@outlook.com>
2024-05-25 15:28:59 +0530

    feat(css_ast): add more methods to `AstBuilder`

--- crates/svelte_oxide_css_ast/src/ast_builder.rs
+++ crates/svelte_oxide_css_ast/src/ast_builder.rs
@@ -70,4 +70,140 @@ impl<'a> AstBuilder<'a> {
     ) -> StyleSheet<'a> {
         StyleSheet { span, children, content: StyleSheetContent { span, styles } }
     }
+
+    #[inline]
+    pub fn at_rule(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        prelude: Atom<'a>,
+        block: Option<Block<'a>>,
+    ) -> AtRule<'a> {
+        AtRule { span, name, prelude, block }
+    }
+
+    #[inline]
+    pub fn style_rule(
+        &self,
+        span: Span,
+        prelude: SelectorList<'a>,
+        block: Block<'a>,
+    ) -> StyleRule<'a> {
+        StyleRule {
+            span,
+            prelude,
+            block,
+            metadata: RuleMetadata { has_local_selectors: false, is_global_block: false },
+        }
+    }
+
+    #[inline]
+    pub fn block(&self, span: Span, children: Vec<'a, BlockChild<'a>>) -> Block<'a> {
+        Block { span, children }
+    }
+
+    #[inline]
+    pub fn declaration(&self, span: Span, property: Atom<'a>, value: Atom<'a>) -> Declaration<'a> {
+        Declaration { span, property, value }
+    }
+
+    #[inline]
+    pub fn selector_list(
+        &self,
+        span: Span,
+        children: Vec<'a, ComplexSelector<'a>>,
+    ) -> SelectorList<'a> {
+        SelectorList { span, children }
+    }
+
+    #[inline]
+    pub fn complex_selector(
+        &self,
+        span: Span,
+        children: Vec<'a, RelativeSelector<'a>>,
+    ) -> ComplexSelector<'a> {
+        ComplexSelector { span, children, metadata: ComplexSelectorMetadata { used: false } }
+    }
+
+    #[inline]
+    pub fn relative_selector(
+        &self,
+        span: Span,
+        combinator: Option<Combinator>,
+        selectors: Vec<'a, SimpleSelector<'a>>,
+    ) -> RelativeSelector<'a> {
+        RelativeSelector {
+            span,
+            combinator,
+            selectors,
+            metadata: RelativeSelectorMetadata {
+                is_global: false,
+                is_host: false,
+                root: false,
+                scoped: false,
+            },
+        }
+    }
+
+    #[inline]
+    pub fn nesting_selector(&self, span: Span) -> SimpleSelector<'a> {
+        SimpleSelector::NestingSelector(NestingSelector { span })
+    }
+
+    #[inline]
+    pub fn type_selector(&self, span: Span, name: Atom<'a>) -> SimpleSelector<'a> {
+        SimpleSelector::TypeSelector(TypeSelector { span, name })
+    }
+
+    #[inline]
+    pub fn id_selector(&self, span: Span, name: Atom<'a>) -> SimpleSelector<'a> {
+        SimpleSelector::IdSelector(IdSelector { span, name })
+    }
+
+    #[inline]
+    pub fn class_selector(&self, span: Span, name: Atom<'a>) -> SimpleSelector<'a> {
+        SimpleSelector::ClassSelector(ClassSelector { span, name })
+    }
+
+    #[inline]
+    pub fn pseudo_element_selector(&self, span: Span, name: Atom<'a>) -> SimpleSelector<'a> {
+        SimpleSelector::PseudoElementSelector(PseudoElementSelector { span, name })
+    }
+
+    #[inline]
+    pub fn pseudo_class_selector(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        args: Option<SelectorList<'a>>,
+    ) -> SimpleSelector<'a> {
+        SimpleSelector::PseudoClassSelector(PseudoClassSelector { span, name, args })
+    }
+
+    #[inline]
+    pub fn attribute_selector(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        matcher: Option<AttributeMatcher>,
+        value: Option<Atom<'a>>,
+        flags: Option<Atom<'a>>,
+    ) -> SimpleSelector<'a> {
+        SimpleSelector::AttributeSelector(AttributeSelector { span, name, matcher, value, flags })
+    }
+
+    #[inline]
+    pub fn nth_selector(&self, span: Span, value: Atom<'a>) -> SimpleSelector<'a> {
+        SimpleSelector::NthSelector(NthSelector { span, value })
+    }
+
+    #[inline]
+    pub fn percentage_selector(&self, span: Span, value: Atom<'a>) -> SimpleSelector<'a> {
+        SimpleSelector::PercentageSelector(PercentageSelector { span, value })
+    }
+
+    #[inline]
+    pub fn combinator(&self, span: Span, kind: CombinatorKind) -> Combinator {
+        Combinator { span, kind }
+    }
 }
