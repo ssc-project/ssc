be024782693b3c15b9fbb99ef10d1e4d3d132b10
Dilshad <a-rustacean@outlook.com>
2024-05-26 08:50:16 +0530

    chore: add fuzzer

---
+++ fuzz/fuzz_targets/css_parser.rs
@@ -0,0 +1,13 @@
+#![no_main]
+
+use oxc_allocator::Allocator;
+use svelte_oxide_css_parser::Parser;
+
+libfuzzer_sys::fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = std::str::from_utf8(data) {
+        if s.chars().all(|s| !s.is_control()) {
+            let allocator = Allocator::default();
+            let _ = Parser::new(&allocator, &s).parse();
+        }
+    }
+});
