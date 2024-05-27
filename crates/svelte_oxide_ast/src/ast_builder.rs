6628851a2eb53985a8d98067fa441fee7c2182b8
Dilshad <a-rustacean@outlook.com>
2024-05-27 21:21:20 +0530

    chore(ast): add more methods to the `AstBuilder`

--- crates/svelte_oxide_ast/src/ast_builder.rs
+++ crates/svelte_oxide_ast/src/ast_builder.rs
@@ -1,6 +1,7 @@
 use std::mem;
 
 use oxc_allocator::{Allocator, Box, String, Vec};
+use oxc_ast::ast::{Expression, Program};
 use oxc_span::{Atom, Span};
 use svelte_oxide_css_ast::ast::StyleSheet;
 
@@ -75,7 +76,318 @@ impl<'a> AstBuilder<'a> {
         Root { span, options: None, fragment, css, instance, module, metadata: RootMetadata { ts } }
     }
 
+    #[inline]
     pub fn fragment(&self, nodes: Vec<'a, FragmentNode<'a>>, transparent: bool) -> Fragment<'a> {
         Fragment { nodes, transparent }
+    }
+
+    #[inline]
+    pub fn script(
+        &self,
+        span: Span,
+        context: ScriptContext,
+        content: Program<'a>,
+        attributes: Vec<'a, Attribute<'a>>,
+    ) -> Script<'a> {
+        Script { span, context, content, attributes }
+    }
+
+    #[inline]
+    pub fn style(
+        &self,
+        span: Span,
+        stylesheet: StyleSheet<'a>,
+        attributes: Vec<'a, Attribute<'a>>,
+        content_span: Span,
+        content: Atom<'a>,
+    ) -> Style<'a> {
+        Style {
+            span,
+            attributes,
+            stylesheet,
+            content: StyleContent { span: content_span, styles: content },
+        }
+    }
+
+    #[inline]
+    pub fn component(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::Component(Component { span, name, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn title_element(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::TitleElement(TitleElement { span, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn slot_element(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::SlotElement(SlotElement { span, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn regular_element(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::RegularElement(RegularElement {
+            span,
+            name,
+            attributes,
+            fragment,
+            metadata: RegularElementMetadata { svg: false, has_spread: false, scoped: false },
+        })
+    }
+
+    #[inline]
+    pub fn svelte_body(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::SvelteBody(SvelteBody { span, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn svelte_component(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+        expression: Expression<'a>,
+    ) -> Element<'a> {
+        Element::SvelteComponent(SvelteComponent { span, attributes, fragment, expression })
+    }
+
+    #[inline]
+    pub fn svelte_document(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::SvelteDocument(SvelteDocument { span, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn svelte_element(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+        expression: Expression<'a>,
+    ) -> Element<'a> {
+        Element::SvelteElement(SvelteElement {
+            span,
+            attributes,
+            fragment,
+            expression,
+            metadata: SvelteElementMetadata { svg: false, scoped: false },
+        })
+    }
+
+    #[inline]
+    pub fn svelte_fragment(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::SvelteFragment(SvelteFragment { span, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn svelte_head(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::SvelteHead(SvelteHead { span, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn svelte_options(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::SvelteOptionsRaw(SvelteOptionsRaw { span, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn svelte_self(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::SvelteSelf(SvelteSelf { span, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn svelte_window(
+        &self,
+        span: Span,
+        attributes: Vec<'a, ElementAttribute<'a>>,
+        fragment: Fragment<'a>,
+    ) -> Element<'a> {
+        Element::SvelteWindow(SvelteWindow { span, attributes, fragment })
+    }
+
+    #[inline]
+    pub fn text(&self, span: Span, raw: Atom<'a>) -> Text<'a> {
+        Text { span, data: raw.clone(), raw }
+    }
+
+    #[inline]
+    pub fn attribute(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        value: AttributeValue<'a>,
+    ) -> Attribute<'a> {
+        Attribute { span, name, value }
+    }
+
+    #[inline]
+    pub fn spread_attribute(&self, span: Span, expression: Expression<'a>) -> SpreadAttribute<'a> {
+        SpreadAttribute {
+            span,
+            expression,
+            metadata: SpreadAttributeMetadata { contains_call_expression: false, dynamic: false },
+        }
+    }
+
+    #[inline]
+    pub fn expression_tag(&self, span: Span, expression: Expression<'a>) -> ExpressionTag<'a> {
+        ExpressionTag {
+            span,
+            expression,
+            metadata: ExpressionTagMetadata { contains_call_expression: false, dynamic: false },
+        }
+    }
+
+    #[inline]
+    pub fn animate_directive(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        expression: Option<Expression<'a>>,
+    ) -> Directive<'a> {
+        Directive::AnimateDirective(AnimateDirective { span, name, expression })
+    }
+
+    #[inline]
+    pub fn bind_directive(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        expression: BindDirectiveExpression<'a>,
+    ) -> Directive<'a> {
+        Directive::BindDirective(BindDirective { span, name, expression })
+    }
+
+    #[inline]
+    pub fn class_directive(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        expression: Expression<'a>,
+    ) -> Directive<'a> {
+        Directive::ClassDirective(ClassDirective {
+            span,
+            name,
+            expression,
+            metadata: ClassDirectiveMetadata { dynamic: false },
+        })
+    }
+
+    #[inline]
+    pub fn let_directive(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        expression: Option<LetDirectiveExpression<'a>>,
+    ) -> Directive<'a> {
+        Directive::LetDirective(LetDirective { span, name, expression })
+    }
+
+    #[inline]
+    pub fn on_directive(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        expression: Option<Expression<'a>>,
+        modifiers: Vec<'a, Atom<'a>>,
+    ) -> Directive<'a> {
+        Directive::OnDirective(OnDirective { span, name, expression, modifiers })
+    }
+
+    #[inline]
+    pub fn style_directive(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        value: AttributeValue<'a>,
+        modifiers: Vec<'a, StyleDirectiveModifier>,
+    ) -> Directive<'a> {
+        Directive::StyleDirective(StyleDirective {
+            span,
+            name,
+            value,
+            modifiers,
+            metadata: StyleDirectiveMetadata { dynamic: false },
+        })
+    }
+
+    #[inline]
+    pub fn transition_directive(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        expression: Option<Expression<'a>>,
+        modifiers: Vec<'a, TransitionDirectiveModifier>,
+        intro: bool,
+        outro: bool,
+    ) -> Directive<'a> {
+        Directive::TransitionDirective(TransitionDirective {
+            span,
+            name,
+            expression,
+            modifiers,
+            intro,
+            outro,
+        })
+    }
+
+    #[inline]
+    pub fn use_directive(
+        &self,
+        span: Span,
+        name: Atom<'a>,
+        expression: Option<Expression<'a>>,
+    ) -> Directive<'a> {
+        Directive::UseDirective(UseDirective { span, name, expression })
     }
 }
