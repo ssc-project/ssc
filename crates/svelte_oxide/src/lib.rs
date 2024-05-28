d8462fed40c32330d6a1eeb2f7a46d2a147926f1
Dilshad <a-rustacean@outlook.com>
2024-05-28 22:12:01 +0530

    chore: re-export items of all crates from the umbrella crate `svelte-oxide`

--- crates/svelte_oxide/src/lib.rs
+++ crates/svelte_oxide/src/lib.rs
@@ -1,1 +1,47 @@
+//! # Svelte Oxide
+//!
+//! <https://github.com/a-rustacean/svelte-oxide>
 
+pub mod allocator {
+    #[doc(inline)]
+    pub use oxc_allocator::*;
+}
+
+pub mod span {
+    #[doc(inline)]
+    pub use oxc_span::*;
+}
+
+pub mod ast {
+    #[doc(inline)]
+    pub use svelte_oxide_ast::*;
+}
+
+pub mod parser {
+    #[doc(inline)]
+    pub use svelte_oxide_parser::*;
+}
+
+#[cfg(feature = "css")]
+pub mod css_ast {
+    #[doc(inline)]
+    pub use svelte_oxide_css_ast::*;
+}
+
+#[cfg(feature = "css")]
+pub mod css_parser {
+    #[doc(inline)]
+    pub use svelte_oxide_css_parser::*;
+}
+
+#[cfg(feature = "css")]
+pub mod css_codegen {
+    #[doc(inline)]
+    pub use svelte_oxide_css_codegen::*;
+}
+
+#[cfg(feature = "css")]
+pub mod css_transformer {
+    #[doc(inline)]
+    pub use svelte_oxide_css_transformer::*;
+}
