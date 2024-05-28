296c7800a0833d482e480ee558ac25a59b1bb125
Dilshad <a-rustacean@outlook.com>
2024-05-28 13:23:08 +0530

    feat(parser): add `parse_js_variable_declarator` method on `ParserImpl`

--- crates/svelte_oxide_parser/src/js.rs
+++ crates/svelte_oxide_parser/src/js.rs
@@ -1,4 +1,4 @@
-use oxc_ast::ast::{Expression, IdentifierReference};
+use oxc_ast::ast::{Expression, IdentifierReference, VariableDeclarationKind, VariableDeclarator};
 use oxc_diagnostics::Result;
 use oxc_span::{GetSpan, SourceType};
 
@@ -28,6 +28,25 @@ impl<'a> ParserImpl<'a> {
         );
         let start_pos = self.lexer.source.position();
         let identifier = parser.parse_identifier_from_position(self.cur_token().start)?;
+        self.lexer.source.set_position(unsafe {
+            start_pos.add((identifier.span.end - self.lexer.offset()) as usize)
+        });
+        self.bump_any();
+        Ok(identifier)
+    }
+
+    pub(crate) fn parse_js_variable_declarator(
+        &mut self,
+        kind: VariableDeclarationKind,
+    ) -> Result<VariableDeclarator<'a>> {
+        let parser = oxc_parser::Parser::new(
+            self.allocator,
+            self.source_text,
+            SourceType::default().with_typescript(self.ts),
+        );
+        let start_pos = self.lexer.source.position();
+        let identifier =
+            parser.parse_variable_declarator_from_position(self.cur_token().start, kind)?;
         self.lexer.source.set_position(unsafe {
             start_pos.add((identifier.span.end - self.lexer.offset()) as usize)
         });
